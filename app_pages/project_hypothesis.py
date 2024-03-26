a57c86bcb8f9087cc33383ca28643406b0140302
Rebaz Pappiland <rebazp@gmail.com>
2024-03-26 12:21:27 +0000

    setup project_hypothesis.py

---
+++ app_pages/project_hypothesis.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+
+def page_project_hypothesis_body():
+    st.write("### Project Hypothesis and Validation")
+
+    st.success(
+        f"*Cherry leaves inficted by powdery mildew shows visual patterns compared to healthy uninfected cherry leaves."
+        f"These patterns are typically on the edges of the leaf, compared to a healthy leaf with no patterns. \n\n"
+        f"*An Image Montage shows typical patterns on the edges."
+        f"Validate the hypothesis by conducting exploratory data analysis (EDA) on the dataset."
+        f"Visualize samples of healthy and infected cherry leaves to identify any noticeable patterns or differences"
+    )
