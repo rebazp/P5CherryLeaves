35ec4002eb0bf57228f97c7955420ae2a5d770f4
Rebaz Pappiland <rebazp@gmail.com>
2024-03-26 13:04:56 +0000

    fix text in project_hypothesis

--- app_pages/project_hypothesis.py
+++ app_pages/project_hypothesis.py
@@ -5,9 +5,9 @@ def page_project_hypothesis_body():
     st.write("### Project Hypothesis and Validation")
 
     st.success(
-        f"*Cherry leaves inficted by powdery mildew shows visual patterns compared to healthy uninfected cherry leaves."
+        f"* Cherry leaves inficted by powdery mildew shows visual patterns compared to healthy uninfected cherry leaves. "
         f"These patterns are typically on the edges of the leaf, compared to a healthy leaf with no patterns. \n\n"
-        f"*An Image Montage shows typical patterns on the edges."
-        f"Validate the hypothesis by conducting exploratory data analysis (EDA) on the dataset."
-        f"Visualize samples of healthy and infected cherry leaves to identify any noticeable patterns or differences"
+        f"* An Image Montage shows typical patterns on the edges."
+        f"Validate the hypothesis by conducting exploratory data analysis (EDA) on the dataset. "
+        f"Visualize samples of healthy and infected cherry leaves to identify any noticeable patterns or differences."
     )
